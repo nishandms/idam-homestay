019f4192aaf3a977d3e53775fe7885fd4e28cc03
nishandms <nishandms06@gmail.com>
2024-12-20 01:30:25 +0530

    addded footer

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 
-$(document).ready(function() {
+$(document).ready(function () {
+  adjustSize();
+
+  $(window).resize(() => {
+    adjustSize();
+  })
+});
+
+function adjustSize() {
   let slidePerView = 4;
   console.log($(window).width())
-  if($(window).width() < 991) {
-    slidePerView = 1
-  } else if($(window).width() < 1500 ) {
+  if ($(window).width() <= 756) {
+    slidePerView = 1;
+    $('.contacts').addClass('order-1');
+  } else if ($(window).width() < 991) {
+    slidePerView = 1;
+    $('.contacts').removeClass('order-1');
+  } else if ($(window).width() < 1500) {
     slidePerView = 2
+    $('.contacts').removeClass('order-1');
   }
   const swiper = new Swiper('.swiper', {
     speed: 400,
@@ -16,6 +29,6 @@ $(document).ready(function() {
       prevEl: '.swiper-button-prev',
     },
   });
-});
+}
 
 
